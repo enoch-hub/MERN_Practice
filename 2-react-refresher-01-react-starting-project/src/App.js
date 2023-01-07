4c85f86469fa029bc2d17b2309cd543d2224c264
enoch-hub <91972378+enoch-hub@users.noreply.github.com>
2023-01-07 21:02:45 +0530

    Add files via upload

--- 2-react-refresher-01-react-starting-project/src/App.js
+++ 2-react-refresher-01-react-starting-project/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the course'}
+  ]
+
+
+  return (
+    <div className='cours-goals'>
+      <h2>Course Goals</h2>
+      
+    </div>
+  );
 };
 
 export default App;
---
+++ 2-react-refresher-01-react-starting-project/src/components/GoalList.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className='goal-list'>
+
+    </ul>
+  )
+}
+
+export default GoalList
+
+// HOw we can output a list 
+// of data dynamically in jsx 
+
+
+
+// We can't output plain vanilla js objects in jsx, instead we need to map every object into a jsx equavilant, so into a renderable element.
