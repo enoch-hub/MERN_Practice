4b686d753a2f886ea1e339e3a9ad2df6f57137da
Enoch <enoch.meshach.perera@gmail.com>
2023-01-12 11:27:43 +0530

    PC

---
+++ react-frontend-01-starting-setup/src/user/components/UsersList.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './UsersList.css' 
+
+const UsersList = props => {
+  return (
+    <li className="user-item">
+        
+    </li>
+  )
+}
+
+export default UsersList
--- react-frontend-01-starting-setup/src/user/pages/Users.jsx
+++ react-frontend-01-starting-setup/src/user/pages/Users.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const Users = () => {
-  return (
-    <div>Users</div>
-  )
-}
+  const USERS = [
+    {
+      id: 'u1',
+      Book: 'Psalms',
+      Chapter: '23',
+      Verse: '1',
+      image:
+        'https://images.pexels.com/photos/3408744/pexels-photo-3408744.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    },
+  ];
 
-export default Users
+  return <div>Users</div>;
+};
+
+export default Users;
